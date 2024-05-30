c977d9c3bc93eead2361ad45e6d9c641385c678b
LA6154 <148690754+LA6154@users.noreply.github.com>
2024-05-30 21:20:32 +0530

    okpl

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 let get = document.getElementById("get");
-
 async function myFun(){
     let getting= await fetch('https://icanhazdadjoke.com/',{
         headers:{
-            accept: "application/json"
+            accept:"application/json"
         }
     })
-    let joke= await getting.json();
-    let data =joke.joke;
+    let rk = await getting.json();
+    let data =rk.joke;
     get.innerHTML=data;
     console.log(data);
 }
